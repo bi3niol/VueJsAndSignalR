caefabb38be867016dce9b4f0175514c8e9b6bad
Bi3NioL <bi3niol@gmail.com>
2018-08-01 00:48:15 +0200

    initial

---
+++ SignalR/SignalR.WebServer/Content/App.js
@@ -0,0 +1,13 @@
+﻿if (window.Client==undefined) {
+	Client = { __namespace__: true };
+}
+if (!window.Client.App) {
+	Client.App = function (element) {
+		return new Vue({
+			el: element,
+			data: {
+				msg: "test xD"
+			}
+		});
+	};
+}
